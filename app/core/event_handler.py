7d2fadb529d1a6f495763835e77f6d5c633a4ff8
rudolf <rudolf.degeijter@meemoo.be>
2021-02-17 13:53:41 +0100

    DEV-1490 Query MH for original metadata using PID.

--- app/core/event_handler.py
+++ app/core/event_handler.py
@@ -6,9 +6,18 @@ import asyncio
 
 async def handle_event(premis_event: PremisEvent) -> None:
     config = ConfigParser().app_cfg
-
     mediahaven_service = MediahavenService(config)
 
+    # Get metadata for the newly archived item
     fragment = mediahaven_service.get_fragment(premis_event.fragment_id)
+    original_pid = fragment["Dynamic"]["dc_source"].split(".")[0]
 
-    print(fragment)
+    print(original_pid)
+    # Query mediahaven for the original item using PID
+    original_metadata = mediahaven_service.query([("PID", original_pid)])
+
+    print(original_metadata.text)
+    # Transform the metadata to a new sidecar
+
+    # Update the newly archived item
+
