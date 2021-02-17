4411801610b457906ad1071c7349955c0ef56b1f
rudolf <rudolf.degeijter@meemoo.be>
2021-02-17 21:53:42 +0100

    DEV-1491 Call metadata transformation.

--- app/core/event_handler.py
+++ app/core/event_handler.py
@@ -1,5 +1,6 @@
 from app.core.events_parser import PremisEvent
 from app.services.mediahaven import MediahavenService
+from app.core.xml_transformer import transform_mh_result_to_sidecar
 from viaa.configuration import ConfigParser
 import asyncio
 
@@ -12,12 +13,11 @@ async def handle_event(premis_event: PremisEvent) -> None:
     fragment = mediahaven_service.get_fragment(premis_event.fragment_id)
     original_pid = fragment["Dynamic"]["dc_source"].split(".")[0]
 
-    print(original_pid)
     # Query mediahaven for the original item using PID
     original_metadata = mediahaven_service.query([("PID", original_pid)])
 
-    print(original_metadata.text)
     # Transform the metadata to a new sidecar
+    sidecar = transform_mh_result_to_sidecar(original_metadata)
 
+    print(sidecar)
     # Update the newly archived item
-
