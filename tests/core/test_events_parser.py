14e80852ea1f25d4653a0feb53ce1f64c3818eee
rudolf <rudolf.degeijter@meemoo.be>
2021-02-24 09:52:46 +0100

    DEV-1508 rewrite tests

--- tests/core/test_events_parser.py
+++ tests/core/test_events_parser.py
@@ -5,7 +5,7 @@ import pytest
 from lxml.etree import XMLSyntaxError
 from lxml.etree import fromstring as parse_xml_string
 
-from app.core.events_parser import PremisEvent, PremisEvents
+from app.core.events_parser import parse_premis_events, parse_premis_event, _get_xpath_from_event
 from tests.resources import (
     invalid_xml_event,
     multi_premis_event,
@@ -30,58 +30,57 @@ from tests.resources import (
     ],
 )
 def test_single_event(resource, event_type):
-    p = PremisEvents(resource)
-    assert len(p.events) == 1
-    assert p.events[0].event_id == "111"
-    assert p.events[0].event_detail == "Ionic Defibulizer"
-    assert p.events[0].fragment_id == "a1b2c3"
-    assert p.events[0].event_type == event_type
-    assert p.events[0].event_outcome == "OK"
-    assert p.events[0].event_datetime == "2019-03-30T05:28:40Z"
-    assert p.events[0].external_id == "a1"
-    assert p.events[0].is_valid
-    assert p.events[0].has_valid_outcome
+    p = parse_premis_events(resource)
+    assert len(p["events"]) == 1
+    assert p["events"][0]["event_id"] == "111"
+    assert p["events"][0]["event_detail"] == "Ionic Defibulizer"
+    assert p["events"][0]["fragment_id"] == "a1b2c3"
+    assert p["events"][0]["event_type"] == event_type
+    assert p["events"][0]["event_outcome"] == "OK"
+    assert p["events"][0]["event_datetime"] == "2019-03-30T05:28:40Z"
+    assert p["events"][0]["external_id"] == "a1"
+    assert p["events"][0]["is_valid"]
+    assert p["events"][0]["has_valid_outcome"]
 
 
 def test_single_event_nok():
-    p = PremisEvents(single_premis_event_nok)
-    assert len(p.events) == 0
+    p = parse_premis_events(single_premis_event_nok)
+    assert len(p["events"]) == 0
 
 
 def test_multi_event():
-    p = PremisEvents(multi_premis_event)
-    assert len(p.events) == 2
-    assert p.events[0].event_id == "333"
-    assert p.events[0].event_detail == "Ionic Defibulizer"
-    assert p.events[0].fragment_id == "d4e5f6"
-    assert p.events[0].event_type == "FLOW.ARCHIVED"
-    assert p.events[0].event_outcome == "OK"
-    assert p.events[0].event_datetime == "2019-03-30T05:28:40Z"
-    assert p.events[0].is_valid
-    assert p.events[0].has_valid_outcome
-    assert p.events[1].event_id == "444"
-    assert p.events[1].event_detail == "Ionic Defibulizer 2"
-    assert p.events[1].fragment_id == "g7h8j9"
-    assert p.events[1].event_type == "RECORDS.FLOW.ARCHIVED"
-    assert p.events[1].event_outcome == "OK"
-    assert p.events[1].event_datetime == "2019-03-30T05:28:40Z"
-    assert p.events[1].is_valid
-    assert p.events[1].has_valid_outcome
+    p = parse_premis_events(multi_premis_event)
+    assert len(p["events"]) == 2
+    assert p["events"][0]["event_id"] == "333"
+    assert p["events"][0]["event_detail"] == "Ionic Defibulizer"
+    assert p["events"][0]["fragment_id"] == "d4e5f6"
+    assert p["events"][0]["event_type"] == "FLOW.ARCHIVED"
+    assert p["events"][0]["event_outcome"] == "OK"
+    assert p["events"][0]["event_datetime"] == "2019-03-30T05:28:40Z"
+    assert p["events"][0]["is_valid"]
+    assert p["events"][0]["has_valid_outcome"]
+    assert p["events"][1]["event_id"] == "444"
+    assert p["events"][1]["event_detail"] == "Ionic Defibulizer 2"
+    assert p["events"][1]["fragment_id"] == "g7h8j9"
+    assert p["events"][1]["event_type"] == "RECORDS.FLOW.ARCHIVED"
+    assert p["events"][1]["event_outcome"] == "OK"
+    assert p["events"][1]["event_datetime"] == "2019-03-30T05:28:40Z"
+    assert p["events"][1]["is_valid"]
+    assert p["events"][1]["has_valid_outcome"]
 
 
 def test_invalid_xml_event():
     with pytest.raises(XMLSyntaxError):
-        PremisEvents(invalid_xml_event)
+        parse_premis_events(invalid_xml_event)
 
 
 def test_single_event_no_external_id():
-    p = PremisEvents(single_event_no_external_id)
-    assert not p.events
+    p = parse_premis_events(single_event_no_external_id)
+    assert not p["events"]
 
 
 def test_get_xpath_from_event():
     input_xml = "<xml><path>value</path></xml>"
     tree = parse_xml_string(input_xml)
-    p = PremisEvent(tree)
-    assert p._get_xpath_from_event("no_such_path") == ""
-    assert p._get_xpath_from_event("path") == "value"
+    assert _get_xpath_from_event(tree, "no_such_path") == ""
+    assert _get_xpath_from_event(tree, "path") == "value"
