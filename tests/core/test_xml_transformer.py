c2b4047fd23db9ee3246699211d4f387ee66e5c4
rudolf <rudolf.degeijter@meemoo.be>
2021-04-22 12:21:57 +0200

    DEV-1517 Add tests for multiple items for PID.

--- tests/core/test_xml_transformer.py
+++ tests/core/test_xml_transformer.py
@@ -1,11 +1,11 @@
 from app.core.xml_transformer import transform_mh_result_to_sidecar
-from tests.resources import query_result_single_result, sidecar
+from tests.resources import fragment_info_xml, sidecar
 
 
 def test_handle_events() -> None:
     # ARRANGE
 
     # ACT
-    transformed = transform_mh_result_to_sidecar(query_result_single_result)
+    transformed = transform_mh_result_to_sidecar(fragment_info_xml)
     # ASSERT
     assert transformed == sidecar
