b2c9e8672146708a8b2e8f9b4477615d1437630c
Maarten <maartendeschrijver@gmail.com>
2021-05-04 15:48:58 +0200

    DEV-1616 Move selection logic to function
    - move selection logic to it's own function
    - add tests for new function
    - add new resource test file for MH response with 3 results
    - modify existing resource: add object types

--- app/core/event_handler.py
+++ app/core/event_handler.py
@@ -10,6 +10,54 @@ config = ConfigParser()
 log = logging.get_logger(__name__, config=config)
 
 
+def determine_original_item(mediahaven_result: dict) -> str:
+    """From a MediaHaven result list, determine which of the items carries the
+    original metadata.
+
+    A MediaHaven-query by pid can result in 0, 1 or more results. Depending on
+    the "structure" of the resultset (ie., the relations between the different
+    items) we need to choose either the main fragment or a real fragment.
+    The - proposed - heuristic is:
+    - if only one result, return
+    - if more then one result: filter out `type=document` (ie., the collaterals)
+        - if only one item left: return
+        - if more then one item left: return main fragment
+
+    Args:
+        The MediaHaven resultset as dict
+    Returns:
+        The correct fragment ID as string, or an empty string if none found.
+    """
+    # Init `original_fragment_id` to the empty string (making the search
+    # exhaustive and thus avoiding explicitly handling the case of an empty
+    # resultset)
+    original_fragment_id = ""
+    # Simple case: if there is only one result, this is the one. No questions
+    # asked.
+    if mediahaven_result["TotalNrOfResults"] == 1:
+        original_fragment_id = mediahaven_result["MediaDataList"][0]["Internal"]["FragmentId"]
+    # If there are multiple items for the PID, we filter out the "documents"
+    # and see what's left
+    elif mediahaven_result["TotalNrOfResults"] > 1:
+        records_minus_documents = [
+            item for item in mediahaven_result["MediaDataList"] if not
+            item["Administrative"]["Type"] == "document"
+        ]
+        # If there's only one record
+        if len(records_minus_documents) == 1:
+            original_fragment_id = records_minus_documents[0]["Internal"]["FragmentId"]
+        # If there's more then one record left we pick the "main fragment",
+        # regardless of it's type. (Theoretically, zero records could remain
+        # if all were of type "document".)
+        else:
+            original_fragment_id = next(
+                (item["Internal"]["FragmentId"] for
+                item in records_minus_documents
+                if not item["Internal"]["IsFragment"])
+            )
+    return original_fragment_id
+
+
 async def handle_event(premis_event: PremisEvent) -> None:
     log.debug(
         "Start handling of PREMIS event.",
@@ -53,18 +101,10 @@ async def handle_event(premis_event: PremisEvent) -> None:
         )
         return
 
-    # If there are multiple items for the PID, we need the one that isn't a fragment
-    if result["TotalNrOfResults"] > 1:
-        original_fragment_id = next(
-            (
-                item["Internal"]["FragmentId"]
-                for item in result["MediaDataList"]
-                if not item["Internal"]["IsFragment"]
-            )
-        )
-    elif result["TotalNrOfResults"] == 1:
-        original_fragment_id = result["MediaDataList"][0]["Internal"]["FragmentId"]
-    else:
+    # Determine the correct fragment ID from the result
+    original_fragment_id = determine_original_item(result)
+
+    if not original_fragment_id:
         log.warning(
             "No item found for original_pid.",
             mediahaven_id=premis_event.mediahaven_id,
