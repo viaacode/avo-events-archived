177361d99c8bb91342dd6ea1f28a45a9bd5fb0de
Mattias.Poppe <poppe.mattias@gmail.com>
2023-07-26 13:31:41 +0200

    DEV-2324 Use MediaHaven REST API V2
    Make use of the MediaHaven Python library.
    
    Make sure that the MediaHaven client only gets instantiated once and reused
    during the entire lifecycle of the app.
    
    Other changes:
    - Add MediaHaven Python library as requirement.
    - The library abstracts the requests.HTTPError. Catch the respective
      MediaHavenException instead.
    - An object instead of a dict is returned as result when requesting record(s)
      from MediaHaven. So catch AttributeError instead of KeyError.
    - Add OAuth client ENV vars to config.yml and .env.example.

--- app/api/routers/event.py
+++ app/api/routers/event.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, BackgroundTasks, Depends
 from viaa.configuration import ConfigParser
 from viaa.observability import logging
+from mediahaven import MediaHaven
 
 from app.core.event_handler import handle_event
 from app.core.events_parser import parse_premis_events
@@ -13,10 +14,17 @@ config = ConfigParser()
 log = logging.get_logger(__name__, config=config)
 
 
+def get_mediahaven_client():
+    from app.app import _mediahaven_client
+
+    return _mediahaven_client
+
+
 @router.post("/", status_code=202)
 async def handle_events(
     background_tasks: BackgroundTasks,
     premis_events: PremisEvents = Depends(XmlBody(PremisEvents, parse_premis_events)),
+    mh_client: MediaHaven = Depends(get_mediahaven_client),
 ):
     """
     Returns OK if the xml parsing didn't crash.
@@ -32,7 +40,7 @@ async def handle_events(
     )
 
     for event in archived_events:
-        background_tasks.add_task(handle_event, event)
+        background_tasks.add_task(handle_event, event, mh_client)
 
     return {
         "message": f"Updating {len(archived_events)} item(s) with metadata from the original fragment in the background."
--- app/app.py
+++ app/app.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+
 from viaa.configuration import ConfigParser
 from viaa.observability import logging
+from mediahaven import MediaHaven
+from mediahaven.oauth2 import RequestTokenError, ROPCGrant
 
 from app.api.api import api_router
 
@@ -10,10 +13,25 @@ app.include_router(api_router)
 
 config = ConfigParser()
 log = logging.get_logger(__name__, config=config)
+_mediahaven_client: MediaHaven = None
 
 
 @app.on_event("startup")
 async def startup_event():
+    global _mediahaven_client
+    mediahaven_config = config.app_cfg["mediahaven"]
+    client_id = mediahaven_config["client_id"]
+    client_secret = mediahaven_config["client_secret"]
+    user = mediahaven_config["username"]
+    password = mediahaven_config["password"]
+    url = mediahaven_config["host"]
+    grant = ROPCGrant(url, client_id, client_secret)
+    try:
+        grant.request_token(user, password)
+    except RequestTokenError as e:
+        log.error(e)
+        raise e
+    _mediahaven_client = MediaHaven(url, grant)
     log.info("avo-events-archived is now accepting requests.")
 
 
