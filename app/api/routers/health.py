cc75f61f3e0cb2bb44bab621f284da7f83caa424
Maarten <maartendeschrijver@gmail.com>
2026-05-05 13:47:45 +0200

    DEV-2784 Add logging to trivial health-call

--- app/api/routers/health.py
+++ app/api/routers/health.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter
+#
+from viaa.configuration import ConfigParser
+from viaa.observability import logging
 
 router = APIRouter()
 
+config = ConfigParser()
+log = logging.get_logger(__name__, config=config)
 
 @router.get("/live")
 async def liveness_check():
     """
     Returns OK if the service is running.
     """
+    log.debug("Health/live called")
     return "OK"
